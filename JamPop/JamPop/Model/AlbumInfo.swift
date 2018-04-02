dc29169d751505002e84484ea91d2ed1cfde8f60
Jakob <jadppf@mail.missouri.edu>
2018-04-02 15:15:50 -0500

    Moved some things into groups

---
+++ JamPop/JamPop/Model/AlbumInfo.swift
@@ -0,0 +1,15 @@
+//
+//  AlbumInfo.swift
+//  JamPop
+//
+//  Created by Jakob Daugherty on 4/2/18.
+//  Copyright © 2018 Jakob Daugherty. All rights reserved.
+//
+
+import Foundation
+
+struct AlbumInfo {
+    var name: String?
+    var artist: String?
+    //var coverArt: 
+}
